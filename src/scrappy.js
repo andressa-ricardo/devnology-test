26f247a0db1e88bb928273bfb57fc12e1326de1b
andressa-ricardo <andressaricardo30@gmail.com>
2024-03-15 18:44:43 -0300

    feat: fetching data to the specified brand

--- src/scrappy.js
+++ src/scrappy.js
@@ -1,10 +1,51 @@
 import puppeteer from "puppeteer";
 
-async function scrappy() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops");
-    await browser.close();
+async function scrappy(marca) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
+  );
+
+  const notebooks = await page.evaluate(function (marca) {
+    const all = document.querySelectorAll(".card-body");
+    const products = [];
+
+    for (let i = 0; i < all.length; i++) {
+      // Obter o título do produto
+      const title = all[i].querySelector("a").title.toLowerCase(); // pegando o titulo pelo elemento title e nao pela classe
+      // Verificar se o título do produto contém a marca especificada
+      if (title.includes(marca.toLowerCase())) {
+        const image = all[i].querySelector("img").src || "";
+        const href =
+          "https://webscraper.io" +
+            all[i].querySelector("a").getAttribute("href") || "";
+        const description = all[i].querySelector(".description").innerText;
+        const price = all[i].querySelector(".price")?.textContent || "";
+        const ratings = all[i].querySelector(".review-count").innerText;
+        const starsContainer = all[i]
+          .querySelector(".ratings")
+          ?.querySelectorAll(".ws-icon-star");
+        const stars = starsContainer ? starsContainer.length : 0;
+
+        products.push({
+          image,
+          href,
+          title,
+          description,
+          price,
+          ratings,
+          stars,
+        });
+      }
+    }
+
+    return products;
+  }, "dell");
+
+  console.log(notebooks);
+
+  await browser.close();
 }
 
-scrappy();
+scrappy("lenovo");
