a5b50af1f7ef60014396bad2842fe02b0461422a
andressa-ricardo <andressaricardo30@gmail.com>
2024-03-17 22:22:43 -0300

    feat: adding data to a json file.

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 import express from "express";
-import cors from "cors";
 import bodyParser from "body-parser";
+import fs from 'fs';
 import { scrappy } from "./src/scrappy.js";
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
   try {
     const result = await scrappy(req.query?.marca || "lenovo");
-    res.send(result);
+    fs.writeFileSync("dados.json", JSON.stringify({result}, null, 2))
+    res.json(result);
+    console.log("Resultados salvos em dados.json");
   } catch (error) {
     console.log("erro ao obter dados", error);
     res.status(500).json({ error: "erro ao obter dados" });
